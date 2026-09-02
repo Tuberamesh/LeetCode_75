aad15e6105da4fdabed7151e93d870df428c17eb
Tuberamesh <r6904633@gmail.com>
2026-09-02 23:16:50 +0530

    25 of 75 Asteroid Collision

--- stack/Stack Problems.java
+++ stack/Stack Problems.java
@@ -1,5 +1,8 @@
 package stack;
 
+//problem name: Remove All Adjacent Duplicates In String
+//time complexity: O(n)
+//space complexity: O(n)
 class Solution {
     public String removeStars(String s) {
 
@@ -21,5 +24,47 @@ class Solution {
         }
 
         return ans.toString();
+    }
+}
+
+
+//problem name: Asteroid Collision
+//time complexity: O(n)
+//space complexity: O(n)
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int ast : asteroids) {
+
+            while (!st.isEmpty() && st.peek() > 0 && ast < 0) {
+
+                if (st.peek() < Math.abs(ast)) {
+                    st.pop();
+
+                } else if (st.peek() == Math.abs(ast)) {
+                    st.pop();
+                    ast = 0;
+                    break;
+
+                } else {
+                    ast = 0;
+                    break;
+                }
+            }
+
+            if (ast != 0) {
+                st.push(ast);
+            }
+        }
+
+        int[] result = new int[st.size()];
+
+        for (int i = 0; i < st.size(); i++) {
+            result[i] = st.get(i);
+        }
+
+        return result;
     }
 }
