6d41e4cfebb271b8f4df0eba3912e8fe0ddfebe2
Tuberamesh <r6904633@gmail.com>
2026-09-03 19:37:15 +0530

    27 of 75 Recounter

---
+++ deque/deque_problems.py
@@ -0,0 +1,19 @@
+#problem no: 933
+#time complexity: O(n)
+#space complexity: O(n)
+
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+
+        return len(self.q)
+        
