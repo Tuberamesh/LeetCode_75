79da9b171d24ffbcaeed726318e51cb028d1de70
Tuberamesh <r6904633@gmail.com>
2026-09-04 15:29:34 +0530

    30 of 75 Odd Even Linked List

--- deque/Linked_list.java
+++ deque/Linked_list.java
@@ -34,3 +34,35 @@ class Solution {
 
 
 }
+
+
+
+
+//problem name: Odd Even Linked List
+//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+}
