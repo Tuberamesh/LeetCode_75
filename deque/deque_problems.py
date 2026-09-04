da564a1333ce60ac5d3d2d6dcfe06dc0686fded6
Tuberamesh <r6904633@gmail.com>
2026-09-04 15:06:28 +0530

    28 of 75  Dota2 Senate

--- deque/deque_problems.py
+++ deque/deque_problems.py
@@ -17,3 +17,34 @@ class RecentCounter:
 
         return len(self.q)
         
+
+
+
+#problem no: 649
+#time complexity: O(n)
+#space complexity: O(n)
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+
+        r = deque()
+        d = deque()
+
+        for i in range(n):
+            if senate[i] == 'R':
+                r.append(i)
+            else:
+                d.append(i)
+
+        while r and d:
+            ri = r.popleft()
+            di = d.popleft()
+
+            if ri < di:
+                r.append(ri + n)
+            else:
+                d.append(di + n)
+
+        return "Radiant" if r else "Dire"
