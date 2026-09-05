4fb7f8b832c4ce5aad392f769a115eea5c0c9cc6
Tuberamesh <r6904633@gmail.com>
2026-09-05 23:42:11 +0530

    31 of 75 reverese linked list

--- deque/Linked_list.java
+++ deque/Linked_list.java
@@ -66,3 +66,28 @@ class Solution {
         return head;
     }
 }
+
+
+
+
+//problem name: Reverse Linked List
+//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode temp=head;
+        ListNode prev=null;
+
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+
+        }
+
+        return prev;
+        
+    }
+}
