305c0caadb257aa8dccc2e73fcc885c82f549a9f
Tuberamesh <r6904633@gmail.com>
2026-09-21 23:30:20 +0530

    #Right Side View of Binary Tree

--- stack/Binary_tree.py
+++ stack/Binary_tree.py
@@ -153,4 +153,29 @@ class Solution:
         
         return right        
 
-    
+
+
+
+#Right Side View of Binary Tree
+#time complexity: O(n)
+#space complexity: O(n)
+
+    class Solution:
+    def rightSideView(self, root):
+
+        result = []
+
+        def dfs(node, depth):
+
+            if node is None:
+                return
+
+            if depth == len(result):
+                result.append(node.val)
+
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
+
+        return result
