5fdf87ecfb3a0c85f052fb728e1055dd93147a98
Tuberamesh <r6904633@gmail.com>
2026-09-21 23:15:34 +0530

    38 of 75 Lowest Common Ancestor of a Binary Tree

--- stack/Binary_tree.py
+++ stack/Binary_tree.py
@@ -125,3 +125,32 @@ class Solution:
         dfs(root, "right", 0)
 
         return ans
+
+
+
+
+    #Lowest Common Ancestor of a Binary Tree
+    #time complexity: O(n)
+    #space complexity: O(n)
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if root is None:
+            return None
+
+        if root==p or root==q:
+            return root
+
+        left=self.lowestCommonAncestor(root.left, p, q)
+        right=self.lowestCommonAncestor(root.right, p, q)
+
+        if left is not None and right is not None:
+            return root
+        
+        if left is not None:
+            return left
+        
+        return right        
+
+    
